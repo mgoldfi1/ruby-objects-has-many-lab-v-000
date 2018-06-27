efd6f96df56ef64145cc3d7e7220f44602bb2c1c
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-27 14:28:32 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,8 +15,8 @@ class Artist
     song.artist = self
   end
 
- def add_song_by_name(name)
-   song = Song.new(name)
+ def add_song_by_name(song_name)
+   song = Song.new(song_name)
    song.artist = self
  end
 
